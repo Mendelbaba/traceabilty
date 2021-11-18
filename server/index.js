4eb6ec6e338b9072d30f7a397a8b99632c9743d8
Mendelbaba <mendybaba@gmail.com>
2021-11-18 15:41:28 -0500

    comment

--- server/index.js
+++ server/index.js
@@ -1,3 +1,4 @@
+require('dotenv').config()
 const express = require('express')
 const app = express()
 const cors = require('cors')
@@ -15,3 +16,10 @@ var rollbar = new Rollbar({
 
 // record a generic message and send it to Rollbar
 rollbar.log('Hello world!')
+
+
+app.use(express.static("client"));  app.get("/", (req, res) => {  
+     res.sendFile("index.html"); 
+    });  
+    const port = process.env.PORT || process.env.SERVER_PORT;
+ app.listen(port, () => console.log(`The port is running on: ${port}`));
